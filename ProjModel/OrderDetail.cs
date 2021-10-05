f9f10d13a2573c877c9ad3ed551f91c595dcb847
unknown <Administrator@KANINI-LTP-469.gisbizdev.gisbiz.chennai.com>
2021-10-05 11:52:56 +0530

    Added JWT with expiry verification

---
+++ ProjModel/OrderDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FlowerStore.ProjModel
+{
+    public partial class OrderDetail
+    {
+        public int OrderId { get; set; }
+        public int? FlowerId { get; set; }
+        public int? CustomerId { get; set; }
+        public int? CartId { get; set; }
+        public double? Totalprice { get; set; }
+        public string Remark { get; set; }
+        public string PaymentStatus { get; set; }
+
+        public virtual Cart Cart { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual Flower Flower { get; set; }
+
+       
+
+    }
+}
---
+++ Views/Sitehome/MyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FlowerStore.ProjModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowerStore.Views.Sitehome
+{
+    public class MyViewModel
+    {
+        public List<Flower> ListA { get; set; }
+        public List<Occasion> ListB { get; set; }
+
+    }
+}
